8872d2e71ddfd4e3d19e64fbb786d353d8eb2fd9
Adam Tuchowski <adam.tuchowski2@wp.pl>
2025-04-27 22:05:20 +0200

    Fixed

--- LABORATORIUM/LAB_1/main.py
+++ LABORATORIUM/LAB_1/main.py
@@ -24,10 +24,15 @@ def save_to_xyz(points, filename):
 
 # Generowanie chmur punktów
 flat_xy = generate_flat_surface_xy(width=10, length=10, num_points=1000)
-flat_xz = generate_flat_surface_xz(width=10, height=5, num_points=1000)
-cylinder = generate_cylindrical_surface(radius=5, height=10, num_points=1000)
 
-# Zapis do plików
-save_to_xyz(flat_xy, "flat_xy.xyz")
-save_to_xyz(flat_xz, "flat_xz.xyz")
-save_to_xyz(cylinder, "cylinder.xyz")
+flat_xz = generate_flat_surface_xz(width=10, height=5, num_points=1000)
+flat_xz[:, 0] += 20  # przesunięcie w osi X
+
+cylinder = generate_cylindrical_surface(radius=5, height=10, num_points=1000)
+cylinder[:, 1] += 20  # przesunięcie w osi Y
+
+# Łączenie wszystkich punktów razem
+all_points = np.vstack((flat_xy, flat_xz, cylinder))
+
+# Zapis do jednego pliku
+save_to_xyz(all_points, "combined.xyz")
